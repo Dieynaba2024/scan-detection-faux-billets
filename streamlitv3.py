01415c0552df44800bff4c669c7e868297b29cd8
Dieynaba2024 <banadieyna@gmail.com>
2025-08-09 13:04:33 +0000

    Update streamlitv3.py

--- streamlitv3.py
+++ streamlitv3.py
@@ -112,7 +112,6 @@ st.markdown("""
 st.markdown("""
 <div class="header">
     <h2 style="color:white; margin:0;">  🔎 Application Scan Franc cfa </h2>
-   
     <p style="color:white; opacity:0.9; margin:10;"> ✅ Solution de détection de faux billets</p>
 </div>
 """, unsafe_allow_html=True)
@@ -266,6 +265,7 @@ if uploaded_file is not None:
             </ul>
         </div>
         """, unsafe_allow_html=True)
+
 
 
 
