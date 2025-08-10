d2d48505d4a75e2662ade81784335f4914b2b9eb
Dieynaba2024 <banadieyna@gmail.com>
2025-08-10 18:12:55 +0000

    Update streamlitv3.py

--- streamlitv3.py
+++ streamlitv3.py
@@ -86,7 +86,7 @@ st.markdown("""
     }
    
     .fake-card {
-        border: 1px var(--danger);
+        border: 1px solid var(--danger);
         border-radius: 50px;
     }
    
@@ -268,6 +268,7 @@ if uploaded_file is not None:
             </ul>
         </div>
         """, unsafe_allow_html=True)
+
 
 
 
